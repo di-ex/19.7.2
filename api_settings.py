15c91b958d76916d40b21ad9e9329dc0feae44aa
di-ex <85092768+di-ex@users.noreply.github.com>
2021-09-06 14:31:31 +0700

    Основы Python

---
+++ api_settings.py
@@ -0,0 +1,17 @@
+# Валидные данные
+valid_email = 'di@mail.com'
+valid_password = '123123'
+
+# Невалидные данные
+invalid_email = 'dlkopop17@mail.com'
+invalid_password = 'a000aghbnvf6753'
+invalid_auth_key = {
+    "key": "ea738148a1f19838e1c5d1413877f3691a3731380e733e877b0ae729"
+}
+empty_auth_key = {
+    "key": ""
+}
+long_name = 'Q'*100000
+kkk = {
+  "key": "a3e8225739a971ab9a1fba5cf721841fb5d62adefd9ef31051ae8f1e"
+}
